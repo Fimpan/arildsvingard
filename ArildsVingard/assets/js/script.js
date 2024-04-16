e79a48a9e486363bbae077e7509e641d34541049
Fim Pruekwatcharakun <thanyapong_96@hotmail.com>
2024-04-16 13:16:30 +0200

    Menu
    changed to drop down menu

--- ArildsVingard/assets/js/script.js
+++ ArildsVingard/assets/js/script.js
@@ -1,16 +1,61 @@
-// https://codepen.io/sutterlity/pen/OJojvW
+//https://codepen.io/Fim-Pruekwatcharakun/pen/KKYxved
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+// dropdown menu
+const navbarMenu = document.getElementById("navbar");
+const burgerMenu = document.getElementById("burger");
+const overlayMenu = document.querySelector(".overlay");
 
-hamburger.addEventListener("click", () => {
-  //Animate Links
-  navLinks.classList.toggle("open");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
-  });
+// Show and Hide Navbar Function
+const toggleMenu = () => {
+   navbarMenu.classList.toggle("active");
+   overlayMenu.classList.toggle("active");
+};
 
-  //Hamburger Animation
-  hamburger.classList.toggle("toggle");
-});
+// Collapsible Mobile Submenu Function
+const collapseSubMenu = () => {
+   navbarMenu
+      .querySelector(".menu-dropdown.active .submenu")
+      .removeAttribute("style");
+   navbarMenu.querySelector(".menu-dropdown.active").classList.remove("active");
+};
+
+// Toggle Mobile Submenu Function
+const toggleSubMenu = (e) => {
+   if (e.target.hasAttribute("data-toggle") && window.innerWidth <= 992) {
+      e.preventDefault();
+      const menuDropdown = e.target.parentElement;
+
+      // If Dropdown is Expanded, then Collapse It
+      if (menuDropdown.classList.contains("active")) {
+         collapseSubMenu();
+      } else {
+         // Collapse Existing Expanded Dropdown
+         if (navbarMenu.querySelector(".menu-dropdown.active")) {
+            collapseSubMenu();
+         }
+
+         // Expanded the New Dropdown
+         menuDropdown.classList.add("active");
+         const subMenu = menuDropdown.querySelector(".submenu");
+         subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+      }
+   }
+};
+
+// Fixed Resize Window Function
+const resizeWindow = () => {
+   if (window.innerWidth > 992) {
+      if (navbarMenu.classList.contains("active")) {
+         toggleMenu();
+      }
+      if (navbarMenu.querySelector(".menu-dropdown.active")) {
+         collapseSubMenu();
+      }
+   }
+};
+
+// Initialize Event Listeners
+burgerMenu.addEventListener("click", toggleMenu);
+overlayMenu.addEventListener("click", toggleMenu);
+navbarMenu.addEventListener("click", toggleSubMenu);
+window.addEventListener("resize", resizeWindow);
