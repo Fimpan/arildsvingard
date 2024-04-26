34361d694520d8078a3b4608c750037b34fdf438
Fim Pruekwatcharakun <thanyapong_96@hotmail.com>
2024-04-26 09:54:27 +0200

    removed unsed code js

--- ArildsVingard/assets/js/script.js
+++ ArildsVingard/assets/js/script.js
@@ -62,63 +62,6 @@ window.addEventListener("resize", resizeWindow);
 
 // _________________________________________________________
 
-//Datepicker
-
-// modal
-// let modal = document.getElementById("myModal");
-// let btn = document.querySelector(".guestModal");
-// let span = document.getElementsByClassName("close")[0];
-// let guestCount = 0;
-// let roomCount = 0;
-
-// btn.onclick = function () {
-//   modal.style.display = "block";
-// };
-
-// span.onclick = function () {
-//   modal.style.display = "none";
-// };
-
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
-// document.getElementById("addGuests").addEventListener("click", function () {
-//   guestCount++;
-//   document.getElementById("guestCount").innerText = guestCount;
-//   document.querySelector(".checkChoose").innerText =
-//     "Antal gäster: " + guestCount;
-// });
-
-// document
-//   .getElementById("subtractGuests")
-//   .addEventListener("click", function () {
-//     if (guestCount > 0) {
-//       guestCount--;
-//       document.getElementById("guestCount").innerText = guestCount;
-//       document.querySelector(".checkChoose").innerText =
-//         "Antal gäster: " + guestCount;
-//     }
-//   });
-
-// document.getElementById("addRooms").addEventListener("click", function () {
-//   roomCount++;
-//   document.getElementById("roomCount").innerText = roomCount;
-//   document.querySelector(".checkChoose").innerText +=
-//     ", Antal rum: " + roomCount;
-// });
-
-// document.getElementById("subtractRooms").addEventListener("click", function () {
-//   if (roomCount > 0) {
-//     roomCount--;
-//     document.getElementById("roomCount").innerText = roomCount;
-//     document.querySelector(".checkChoose").innerText +=
-//       ", Antal rum: " + roomCount;
-//   }
-// });
-
 //calendar
 let myDays;
 let start = "";
