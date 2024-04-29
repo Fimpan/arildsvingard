9555e618d4e5c836400d520015cfad88d7d08ad2
Fim Pruekwatcharakun <thanyapong_96@hotmail.com>
2024-04-29 17:36:06 +0200

    tried to fix collapsible but cant

--- ArildsVingard/assets/js/script.js
+++ ArildsVingard/assets/js/script.js
@@ -1,65 +1,3 @@
-//https://codepen.io/Fim-Pruekwatcharakun/pen/KKYxved
-
-// dropdown menu
-const navbarMenu = document.getElementById("navbar");
-const burgerMenu = document.getElementById("burger");
-const overlayMenu = document.querySelector(".overlay");
-
-// Show and Hide Navbar Function
-const toggleMenu = () => {
-  navbarMenu.classList.toggle("active");
-  overlayMenu.classList.toggle("active");
-};
-
-// Collapsible Mobile Submenu Function
-const collapseSubMenu = () => {
-  navbarMenu
-    .querySelector(".menu-dropdown.active .submenu")
-    .removeAttribute("style");
-  navbarMenu.querySelector(".menu-dropdown.active").classList.remove("active");
-};
-
-// Toggle Mobile Submenu Function
-const toggleSubMenu = (e) => {
-  if (e.target.hasAttribute("data-toggle") && window.innerWidth <= 992) {
-    e.preventDefault();
-    const menuDropdown = e.target.parentElement;
-
-    // If Dropdown is Expanded, then Collapse It
-    if (menuDropdown.classList.contains("active")) {
-      collapseSubMenu();
-    } else {
-      // Collapse Existing Expanded Dropdown
-      if (navbarMenu.querySelector(".menu-dropdown.active")) {
-        collapseSubMenu();
-      }
-
-      // Expanded the New Dropdown
-      menuDropdown.classList.add("active");
-      const subMenu = menuDropdown.querySelector(".submenu");
-      subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-    }
-  }
-};
-
-// Fixed Resize Window Function
-const resizeWindow = () => {
-  if (window.innerWidth > 992) {
-    if (navbarMenu.classList.contains("active")) {
-      toggleMenu();
-    }
-    if (navbarMenu.querySelector(".menu-dropdown.active")) {
-      collapseSubMenu();
-    }
-  }
-};
-
-// Initialize Event Listeners
-burgerMenu.addEventListener("click", toggleMenu);
-overlayMenu.addEventListener("click", toggleMenu);
-navbarMenu.addEventListener("click", toggleSubMenu);
-window.addEventListener("resize", resizeWindow);
-
 // _________________________________________________________
 
 //calendar
@@ -170,4 +108,19 @@ addRange(5, 10);
 // Reset the selected dates
 resetDates();
 
+// _____________________
+
+// collapsible text
+document.querySelector(".expand-button").addEventListener("click", function () {
+  var bookingTextInfo = document.querySelector(".booking-text-info");
+  var expandButton = document.querySelector(".expand-button");
+
+  bookingTextInfo.classList.toggle("-expanded");
+
+  if (bookingTextInfo.classList.contains("-expanded")) {
+    expandButton.innerHTML = "Collapse Content";
+  } else {
+    expandButton.innerHTML = "Continue Reading";
+  }
+});
 // localStorage.setItem("name", true);
