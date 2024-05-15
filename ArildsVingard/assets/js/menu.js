4db995bc3ab8d55d4b3fbe9f26f29bb186255a8b
Fim Pruekwatcharakun <thanyapong_96@hotmail.com>
2024-05-15 16:25:10 +0200

    fixed meny

--- ArildsVingard/assets/js/menu.js
+++ ArildsVingard/assets/js/menu.js
@@ -7,6 +7,8 @@ const overlayMenu = document.querySelector(".overlay");
 
 // Show and Hide Navbar Function
 const toggleMenu = () => {
+  console.log("rr");
+  burgerMenu.classList.toggle("exit");
   navbarMenu.classList.toggle("active");
   overlayMenu.classList.toggle("active");
 };
@@ -24,7 +26,27 @@ const toggleSubMenu = (e) => {
   if (e.target.hasAttribute("data-toggle")) {
     e.preventDefault();
     const menuDropdown = e.target.parentElement;
+    console.log(menuDropdown);
+    // If Dropdown is Expanded, then Collapse It
+    if (menuDropdown.classList.contains("active")) {
+      collapseSubMenu();
+    } else {
+      // Collapse Existing Expanded Dropdown
+      if (navbarMenu.querySelector(".menu-dropdown.active")) {
+        collapseSubMenu();
+      }
 
+      // Expanded the New Dropdown
+      menuDropdown.classList.add("active");
+      const subMenu = menuDropdown.querySelector(".submenu");
+      subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+    }
+  }
+
+  if (e.target.localName === "a" || e.target.localName === "img") {
+    console.log(e.target.parentElement.parentElement);
+    const menuDropdown = e.target.parentElement.parentElement;
+    console.log(menuDropdown);
     // If Dropdown is Expanded, then Collapse It
     if (menuDropdown.classList.contains("active")) {
       collapseSubMenu();
